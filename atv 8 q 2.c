5a8e4389fa432b4717ab3c049483be1e35b76f32
JuliaSilva05 <128699860+JuliaSilva05@users.noreply.github.com>
2023-09-04 15:26:39 -0300

    Add files via upload

---
+++ atv 8 q 2.c
@@ -0,0 +1,15 @@
+//atv 8 q 2
+#include <stdio.h>
+#include <stdlib.h>
+int fatorial(int N)
+{
+    if (N==0){
+        return 1;
+    } else{
+        return N*fatorial(N-1);
+    }
+}
+
+int main(){
+    fatorial;
+}
